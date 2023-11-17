54f4586cab22da13d5e277a317e98ee75ce00321
Arondhan <lifecrusher7@gmail.com>
2023-11-17 20:47:20 +0500

    отзывы(кнопки)

--- components/reviewContainer.tsx
+++ components/reviewContainer.tsx
@@ -45,13 +45,31 @@ export const Reviews = (): JSX.Element => {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {/* Блок с отзывами */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {reviews.map((review, index) => (
-          <ReviewCard key={index} {...review} />
+            <ReviewCard key={index} {...review} />
         ))}
-      </div>
+        </div>
+
+        {/* Кнопки "Оставить отзыв" и "Все отзывы" */}
+        <div className="w-full flex justify-center items-center mt-8">
+        <div className="w-[740px] h-[47px] justify-center items-center gap-5 inline-flex">
+            <button className="w-[360px] h-[47px] relative bg-white rounded-[5px] border border-cyan-700 focus:outline-none">
+            <div className="left-[108px] top-[16px] absolute text-cyan-700 text-sm font-semibold font-inter uppercase leading-[15px] tracking-wide">
+                Оставить отзыв
+            </div>
+            </button>
+            <button className="w-[360px] h-[47px] relative bg-cyan-700 rounded-[5px] border border-cyan-700 focus:outline-none">
+            <div className="left-[128px] top-[16px] absolute text-white text-sm font-semibold font-inter uppercase leading-[15px] tracking-wide">
+                Все отзывы
+            </div>
+            </button>
+        </div>
+        </div>
+
     </div>
+
   );
 };
-
 export default Reviews;
