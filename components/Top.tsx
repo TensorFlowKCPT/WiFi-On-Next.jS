cf2c9582aeccf6f8c8025fc079c9a72fe1715c96
72PikaChu72 <72vladvlad72@gmail.com>
2023-12-01 19:23:31 +0500

    Подсказки к поиску адреса епте

--- components/Top.tsx
+++ components/Top.tsx
@@ -27,6 +27,49 @@ export const Top = (): JSX.Element => {
     setModalIsOpen(false);
   };
 
+  var [suggestions, setSuggestions] = useState([]); // Состояние для хранения предложенных вариантов
+
+  const CheckAdress = (query) => {
+
+    // Ваша логика проверки адреса и получения предложенных вариантов
+    // Например, это может быть запрос к серверу или фильтрация из статического списка
+    var url = "http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
+    var token = "37246a81de5e3317c98fb92126a5e5bf19aae2b2";
+    var options = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": "Token " + token
+      },
+      body: JSON.stringify({query: query, count:5})
+  }
+  fetch(url, options)
+  .then(response => response.text())
+  .then(result => {
+    const suggestions = JSON.parse(result).suggestions.map(suggestion=> suggestion.value);
+    if(suggestions.length===1 && suggestions[0] === query){
+      // Если мы здесь, значит пользователь ввел свой адрес максимально точно
+      console.log("Мы здесь, долбоеб")
+    }
+    setSuggestions(suggestions);
+  })
+  .catch(error => console.log("error", error));
+  };
+  const [inputValue, setInputValue] = useState('');
+  const handleSuggestionClick = (suggestion) => {
+    setInputValue(suggestion);
+    setSuggestions([]); // Закрываем список подсказок после выбора
+    CheckAdress(suggestion);
+  };
+  const handleInput = (event) => {
+    const query = event.target.value;
+    setInputValue(query);
+
+    // Вызываем CheckAdress при каждом вводе пользователя
+    CheckAdress(query);
+  };
   return (
     <div className="flex flex-col md:flex-row items-center justify-center px-4 py-8 bg-neutralsilver">
       <div className="flex flex-col items-center md:text-left md:w-1/2 md:gap-8">
@@ -44,10 +87,27 @@ export const Top = (): JSX.Element => {
         <div className="flex flex-col md:flex-col items-center md:w-full md:gap-4">
           <motion.input
             {...fadeInAnimation}
+            onInput={handleInput}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
             type="text"
             placeholder="Введите адрес"
             className="flex-grow w-full h-12 md:h-[40px] px-4 py-2 rounded-[12px] border-0 bg-white mb-4 md:mb-0"
           />
+          {suggestions.length > 0 && (
+        <ul className="px-4 py-2 cursor-pointer transition-colors duration-300">
+          {suggestions.map((suggestion, index) => (
+            <li 
+            key={index} 
+            className="px-4 py-2 cursor-pointer hover:bg-gray-200"
+            onClick={() => handleSuggestionClick(suggestion)}
+            >
+              {suggestion}
+              
+            </li>
+          ))}
+        </ul>
+      )}
 
           <motion.button
             {...fadeInAnimation}
